366bce6ee42df6206d2d842f3ad06b775eedda29
adrsh <adarshtester7@gmail.com>
2025-05-27 11:36:59 +0530

    "vitiger"

---
+++ Vtiger/src/test/java/AutomateTestCases/sdhcsd.java
@@ -0,0 +1,20 @@
+package AutomateTestCases;
+
+import org.testng.annotations.Test;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class sdhcsd {
+	
+	public void extentReorts() {
+	
+	}
+	
+
+	@Test
+	public void mnas() {
+	System.out.println("hi rahul");		
+		
+		
+	}
+}
